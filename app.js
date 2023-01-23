781694131d3d57cef3c0e7f844abbfaf5ec07c84
taaminz <albin756@gmail.com>
2023-01-24 00:41:13 +0530

    bug fix

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ mongoose.set("strictQuery", false);
 
 app.use(bodyParser.json());
 
-app.use('/uploads/images',express.static(path.join('uploads','images')));
+app.use('/uploads/images',express.static(path.join(__dirname,'uploads','images')));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
